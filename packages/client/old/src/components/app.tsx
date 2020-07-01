de66a34b8d7c10aa5e7258e5ed1545a8b03027d9
nicknauert <nicknauert@protonmail.com>
2020-07-01 16:29:07 -0400

    create-react-app frontend

---
+++ packages/client/old/src/components/app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const App = () => (
+    <div>
+        Life affirming web application goes here.
+    </div>
+)
---
+++ packages/client/src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Life affirming app goes here
+    </div>
+  );
+}
+
+export default App;
